4145999870cfbbedfa3f2f77ecf0e7232d62df30
HEEJUNG <duqwlqdk@gmail.com>
2025-07-02 17:15:17 +0900

    Update Write.js

--- src/component/Write.js
+++ src/component/Write.js
@@ -46,7 +46,7 @@ function Write() {
       const imgValue = Array.isArray(form.img)
           ? form.img[0]?.name || ''
           : form.img;
-        await axios.post('http://localhost:9070/write', {
+        await axios.post('https://port-0-backend-mbeeoks1e1ce5c07.sel4.cloudtype.app/write', {
           ...form,
           img: imgValue, userid
         });
